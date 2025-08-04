cd96ef662de28262fcafdb1178c3356829cd6e86
Flawid DSouza <me@flawiddsouza.com>
2025-08-04 20:20:09 +0530

    change server port from 3000 to 9008

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(9008)
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
